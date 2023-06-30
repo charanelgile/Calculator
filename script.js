e2172ab184fb2c3369cda67df0f530e101352511
Charanel Gile <charanelgile@icloud.com>
2023-06-30 21:56:53 +0800

    Initial Commit
    - Setup boilerplate HTML
    - Link the CSS and JavaScript to the HTML file

---
+++ script.js
@@ -0,0 +1,5 @@
+const initializeCalculatorApp = () => {
+  alert("The Calculator App has been initialized successfully.");
+};
+
+document.addEventListener("DOMContentLoaded", initializeCalculatorApp);
