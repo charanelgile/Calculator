093eab1c7519e1264e1ce88f39019c3d091bcb5d
Charanel Gile <charanelgile@icloud.com>
2023-07-03 01:24:05 +0800

    Declare all the Variables on Top of the Program
    - Fix the History Panel to display a divider
    after a new set of equation sequence has been started
    - Fix the delete button emptying the calculator
    display when the last number is deleted, instead
    of displaying zero

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const initializeCalculatorApp = () => {
-  // Session Date & Time Variables
+  // Variables
   let dateObj = new Date();
   let today = dateObj.getDay();
   let month = dateObj.getMonth();
@@ -8,31 +8,207 @@ const initializeCalculatorApp = () => {
   let hours = dateObj.getHours();
   let minutes = dateObj.getMinutes();
 
+  let lblNickname = document.querySelector(".lblNickname");
+  let nickname = document.querySelector(".nickname");
+
+  let greeting = document.querySelector(".greeting");
+  let session = document.querySelector(".session");
+
+  let formContainer = document.querySelector(".formContainer");
+  let mainContainer = document.querySelector(".mainContainer");
+
   let previousValue = document.querySelector(".prevValue");
   let currentValue = document.querySelector(".currValue");
-  let history = document.querySelector(".history");
+  let newValue = 0;
 
-  let itemsArr = [];
-  let equationArr = [];
-
+  let newOperator = "";
+  let newNumberInput = "";
   let newNumberFlag = false;
 
-  // Display the nickname provided by the user and the session date and time
-  const btnSubmit = document.querySelector(".btnSubmit");
-  btnSubmit.addEventListener("click", () => {
-    let nickname = document.querySelector(".nickname").value;
-    let lblNickname = document.querySelector(".lblNickname");
-    let session = document.querySelector(".session");
+  let stringEquation = ""; // equationStr
 
-    // Check if the nickname field is empty or not
-    checkNickname(nickname, lblNickname);
+  let currentEquation = []; // itemsArr
+  let convertEquation = {}; // equationObj
+  let historyEquations = []; // equationArr
 
-    session.innerText = `Session: ${determineToday(today)} - ${determineMonth(
-      month
-    )} ${date}, ${year} - ${determineTime(hours, minutes)}`;
+  let history = document.querySelector(".history");
+
+  // Operator Keys Handler
+  const operatorKeys = document.querySelectorAll(".operator");
+  operatorKeys.forEach((oprtrKey) => {
+    oprtrKey.addEventListener("click", (event) => {
+      // Cleanup the Current Equation Array everytime a New Operator is selected
+      if (newNumberFlag) {
+        currentEquation = [];
+      }
+
+      newOperator = event.target.innerText;
+
+      // Return last value or number, when an operator key is clicked without a first number
+      if (!currentEquation.length && currentValue.value == 0) return;
+
+      // Begin a new operation
+      if (!currentEquation.length) {
+        // Store the First Number and Operator into Current Equation Array
+        currentEquation.push(currentValue.value, newOperator);
+        previousValue.innerText = `${currentValue.value} ${newOperator}`;
+        // Replace the number on the input box to avoid accidental concatination
+        return (newNumberFlag = true);
+      }
+
+      // Complete the operation
+      if (currentEquation.length) {
+        // Store the Second Number into Current Equation Array
+        currentEquation.push(currentValue.value);
+
+        // Convert the Current Equation Array into an Object
+        convertEquation = {
+          fnum: parseFloat(currentEquation[0]),
+          oper: currentEquation[1],
+          snum: parseFloat(currentEquation[2]),
+        };
+
+        // Store the Converted Equation into the History of Equations
+        historyEquations.push(convertEquation);
+
+        // Concatinate the Equation into a single String
+        stringEquation = `${convertEquation["fnum"]} ${convertEquation["oper"]} ${convertEquation["snum"]}`;
+
+        // Calculate the New Value to be displayed
+        newValue = calculate(stringEquation, currentValue);
+
+        previousValue.innerText = `${newValue} ${newOperator}`;
+
+        // Continuous operation using the result of the previous equation
+        currentEquation = [newValue, newOperator];
+        newNumberFlag = true;
+
+        // Append the Recent Equation to the History Panel
+        let previousEquation = document.createElement("p");
+        previousEquation.innerText = `${convertEquation.fnum}\n${convertEquation.oper} ${convertEquation.snum}\n------------`;
+
+        history.appendChild(previousEquation);
+
+        // console.log(stringEquation);
+        // console.log(currentEquation);
+        // console.log(convertEquation);
+        // console.log(historyEquations);
+      }
+    });
   });
 
-  // Numpad Press Handler
+  // Show the Calculator App, once a Nickname has been provided **********
+  const btnSubmit = document.querySelector(".btnSubmit");
+  btnSubmit.addEventListener("click", () => {
+    // Check if the Nickname field is empty or not
+    if (nickname.value === "") {
+      lblNickname.innerText = "Please enter a nickname";
+      lblNickname.style.color = "rgb(150, 65, 60)";
+    } else {
+      greeting.innerText = `Hello, ${nickname.value}`;
+
+      formContainer.style.display = "none";
+      mainContainer.style.visibility = "visible";
+    }
+  });
+
+  // Get the Session Date & Time ************************************
+  session.innerText = `Session: ${determineToday(today)} - ${determineMonth(
+    month
+  )} ${date}, ${year} - ${determineTime(hours, minutes)}`;
+
+  // Numpad Mouse Click Handler **************************
+  const numberKeys = document.querySelectorAll(".number");
+  numberKeys.forEach((numKey) => {
+    numKey.addEventListener("click", (event) => {
+      newNumberInput = event.target.innerText;
+
+      if (newNumberFlag) {
+        currentValue.value = newNumberInput;
+        newNumberFlag = false;
+      } else {
+        currentValue.value =
+          currentValue.value == 0
+            ? newNumberInput.trim()
+            : `${currentValue.value}${newNumberInput}`;
+      }
+    });
+  });
+
+  // Clear Keyboard Press Handler ******************
+  document.addEventListener("keypress", (event) => {
+    // Alternative for Clear (Ctrl + Backspace)
+    if (event.key === "Delete") {
+      currentValue.value = 0;
+    }
+  });
+
+  // Clear & Clear All Mouse Click Handlers ***********************
+  const clearKeys = document.querySelectorAll(".allClear, .clear");
+  clearKeys.forEach((clrKey) => {
+    clrKey.addEventListener("click", (event) => {
+      currentValue.value = 0;
+      previousValue.innerText = "";
+
+      // Append the result of the Equation Sequence with a Divider
+      // to indicate a New Set of Equations in the History Panel
+      let previousEquation = document.createElement("p");
+      previousEquation.innerText = `${currentEquation[0]}\n============\n`;
+
+      history.appendChild(previousEquation);
+
+      stringEquation = "";
+      currentEquation = [];
+      convertEquation = {};
+
+      console.log(historyEquations);
+
+      if (event.target.classList.contains("allClear")) {
+        currentEquation = [];
+        historyEquations = [];
+        history.innerHTML = "";
+        previousValue.innerText = "";
+
+        // console.log(`Current Equation\n${currentEquation}`);
+        // console.log(`All Time Equation\n${historyEquations}`);
+      }
+    });
+  });
+
+  // Delete Keyboard Press Handler ****************
+  document.addEventListener("keydown", (event) => {
+    if (event.keyCode === 8) {
+      // Slice the last item off the series of characters
+      currentValue.value = currentValue.value.slice(0, -1);
+    }
+
+    if (currentValue.value === "") {
+      currentValue.value = 0;
+    }
+  });
+
+  // Delete Mouse Click Handler **********************
+  const deleteKey = document.querySelector(".delete");
+  deleteKey.addEventListener("click", () => {
+    // Slice the last item off the series of characters
+    currentValue.value = currentValue.value.slice(0, -1);
+
+    if (currentValue.value === "") {
+      currentValue.value = 0;
+    }
+  });
+
+  // Change Sign Mouse Click Handler *************************
+  const changeSignKey = document.querySelector(".changeSign");
+  changeSignKey.addEventListener("click", () => {
+    // Ensure the series of characters is a number with parseFloat()
+    // Multiply by -1 to achieve the Positive to Negative switch, and vice versa
+    currentValue.value = parseFloat(currentValue.value) * -1;
+  });
+
+  // -------------------------------------------------------------------
+
+  // Numpad Keyboard Press Handler
   document.addEventListener("keypress", (event) => {
     if (Number(event.key) >= 0 || Number(event.key) <= 9) {
       if (newNumberFlag) {
@@ -45,130 +221,6 @@ const initializeCalculatorApp = () => {
             : `${currentValue.value}${event.key}`;
       }
     }
-  });
-
-  // Numpad Click Handler
-  const numberKeys = document.querySelectorAll(".number");
-  numberKeys.forEach((key) => {
-    key.addEventListener("click", (event) => {
-      const newInput = event.target.innerText;
-
-      if (newNumberFlag) {
-        currentValue.value = newInput;
-        newNumberFlag = false;
-      } else {
-        currentValue.value =
-          currentValue.value == 0
-            ? newInput.trim()
-            : `${currentValue.value}${newInput}`;
-      }
-    });
-  });
-
-  // Clear Press Handler
-  document.addEventListener("keypress", (event) => {
-    // Alternative for Clear
-    if (event.key === "Delete") {
-      currentValue.value = 0;
-    }
-  });
-
-  // Clear / Clear All Click Handler
-  const clearKeys = document.querySelectorAll(".allClear, .clear");
-  clearKeys.forEach((key) => {
-    key.addEventListener("click", (event) => {
-      currentValue.value = 0;
-
-      if (event.target.classList.contains("allClear")) {
-        previousValue.innerText = "";
-        itemsArr = [];
-      }
-    });
-  });
-
-  // Delete Press Handler
-  document.addEventListener("keydown", (event) => {
-    if (event.keyCode === 8) {
-      currentValue.value = currentValue.value.slice(0, -1);
-    }
-  });
-
-  // Delete Click Handler
-  const deleteKey = document.querySelector(".delete");
-  deleteKey.addEventListener("click", () => {
-    // Slice the last item off the series of characters
-    currentValue.value = currentValue.value.slice(0, -1);
-  });
-
-  // Sign Change Key Handler
-  const changeSignKey = document.querySelector(".changeSign");
-  changeSignKey.addEventListener("click", () => {
-    // Ensure the series of characters is a number with parseFloat()
-    // Multiply by -1 to achieve the Negative Sign
-    currentValue.value = parseFloat(currentValue.value) * -1;
-  });
-
-  // Operator Keys Handler
-  const operatorKeys = document.querySelectorAll(".operator");
-  operatorKeys.forEach((key) => {
-    key.addEventListener("click", (event) => {
-      // Replace the result on the input box (when an operation was just evaluated)
-      // upon click of a new number key
-      if (newNumberFlag) {
-        previousValue.innerText = "";
-        itemsArr = [];
-      }
-
-      const newOperator = event.target.innerText;
-      const currentVal = currentValue.value;
-
-      // Return last value or number, when an operator key is clicked without a first number
-      if (!itemsArr.length && currentVal == 0) return;
-
-      // Begin a new operation
-      if (!itemsArr.length) {
-        itemsArr.push(currentVal, newOperator); // First Number and Operator
-        previousValue.innerText = `${currentVal} ${newOperator}`;
-        // Replace the number on the input box to avoid accidental concatination
-        return (newNumberFlag = true);
-      }
-
-      // Complete the operation
-      if (itemsArr.length) {
-        itemsArr.push(currentVal); // Second Number
-
-        // Equation - Recent
-        const equationObj = {
-          num1: parseFloat(itemsArr[0]),
-          num2: parseFloat(currentVal),
-          oper: itemsArr[1],
-        };
-
-        // Equations - Historical
-        equationArr.push(equationObj);
-
-        // Equation - Concatinated
-        const equationStr = `${equationObj["num1"]} ${equationObj["oper"]} ${equationObj["num2"]}`;
-
-        const newValue = calculate(equationStr, currentValue);
-
-        previousValue.innerText = `${newValue} ${newOperator}`;
-
-        // Continuous operation using the result of the previous equation
-        itemsArr = [newValue, newOperator];
-        newNumberFlag = true;
-        // console.log(equationArr);
-
-        // Append to History Panel
-        let lastEquation = document.createElement("p");
-
-        lastEquation.innerText = `${equationArr.at(-1).num1}\n${
-          equationArr.at(-1).oper
-        } ${equationArr.at(-1).num2}\n--------`;
-
-        history.appendChild(lastEquation);
-      }
-    });
   });
 
   // Calculate Key Handler
@@ -212,33 +264,47 @@ const initializeCalculatorApp = () => {
 
     itemsArr = [];
     newNumberFlag = true;
-    // console.log(equationArr);
 
     // Append to History Panel
     let lastEquation = document.createElement("p");
 
     lastEquation.innerText = `${equationArr.at(-1).num1}\n${
       equationArr.at(-1).oper
-    } ${equationArr.at(-1).num2}\n--------`;
+    } ${equationArr.at(-1).num2}\n----------`;
 
     history.appendChild(lastEquation);
-  });
-};
 
-const checkNickname = (nickname, lblNickname) => {
-  if (nickname) {
-    let greeting = document.querySelector(".greeting");
+    // console.log(itemsArr);
+    // console.log(equationArr);
+    // console.log(equationObj);
+    // console.log(equationStr);
+  });
+
+  // Close the Calculator App
+  const btnClose = document.querySelector(".btnClose");
+  btnClose.addEventListener("click", () => {
     let formContainer = document.querySelector(".formContainer");
     let mainContainer = document.querySelector(".mainContainer");
 
-    greeting.innerText = `Hello, ${nickname}`;
+    let lblNickname = document.querySelector(".lblNickname");
+    let nickname = document.querySelector(".nickname");
+    let greeting = document.querySelector(".greeting");
+    let session = document.querySelector(".session");
 
-    formContainer.style.display = "none";
-    mainContainer.style.visibility = "visible";
-  } else {
-    lblNickname.innerText = "Please enter a nickname";
-    lblNickname.style.color = "rgb(150, 65, 60)";
-  }
+    itemsArr = [];
+    equationArr = [];
+    history.innerHTML = "";
+    currentValue.value = 0;
+    previousValue.innerText = "";
+    nickname.value = "";
+    greeting.innerText = "";
+    session.innerText = "";
+    lblNickname.innerText = "Enter your nickname:";
+    lblNickname.style.color = "black";
+
+    formContainer.style.display = "flex";
+    mainContainer.style.visibility = "hidden";
+  });
 };
 
 const determineToday = (today) => {
@@ -327,30 +393,39 @@ const determineTime = (hours, minutes) => {
     case 0:
       time.hours = "12";
       break;
+    case 1:
     case 13:
       time.hours = "01";
       break;
+    case 2:
     case 14:
       time.hours = "02";
       break;
+    case 3:
     case 15:
       time.hours = "03";
       break;
+    case 4:
     case 16:
       time.hours = "04";
       break;
+    case 5:
     case 17:
       time.hours = "05";
       break;
+    case 6:
     case 18:
       time.hours = "06";
       break;
+    case 7:
     case 19:
       time.hours = "07";
       break;
+    case 8:
     case 20:
       time.hours = "08";
       break;
+    case 9:
     case 21:
       time.hours = "09";
       break;
@@ -398,18 +473,22 @@ const determineTime = (hours, minutes) => {
   return `${time.hours}:${time.minutes} ${time.flagAMorPM}`;
 };
 
-const calculate = (equation, currentValue) => {
+const calculate = (stringEquation, currentValue) => {
   const regex = /(^[*/=])|(\s)/g;
 
-  equation.replace(regex, "");
+  stringEquation.replace(regex, "");
 
-  const divisionByZero = /(\/0)/.test(equation);
+  const divisionByZero = /(\/0)/.test(stringEquation);
 
-  // Return 0 when any number is divided by Zero
+  // Return 0 when any number is divided by Zero ...
   if (divisionByZero) return (currentValue.value = 0);
 
-  // Otherwise, return the result of the evaluated equation
-  return (currentValue.value = eval(equation));
+  if (currentValue.value == "Infinity") {
+    currentValue.value = 0;
+  }
+
+  // ... Otherwise, return the result of the evaluated equation
+  return (currentValue.value = eval(stringEquation));
 };
 
 document.addEventListener("DOMContentLoaded", initializeCalculatorApp);
